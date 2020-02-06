982d7b7b925612ecb4d0695513891fdcf5ce9391
Alexander Tur <alexandertur.fe@gmail.com>
2020-02-06 23:27:12 +0200

    test commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react'
 
 export default class App extends React.Component {
   render() {
+    const test = true
     return (
       <div className="form-container card">
         <form className="form card-body" />
       </div>
-    );
+    )
   }
 }
