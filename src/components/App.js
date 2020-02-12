74345deb2341d739ef108aade4fc76932b4d6299
Alexander Tur <alexandertur.fe@gmail.com>
2020-02-12 09:41:43 +0200

    added switch to checkErrors function

--- src/components/App.js
+++ src/components/App.js
@@ -103,83 +103,77 @@ export default class App extends React.Component {
     const errors = {}
     const mailRe = /\S+@\S+\.\S+/
     const telRe = /^([+]\d{2})?\d{10}$/
-    if (
-      this.state.currentStep === 1 &&
-      this.state.values.firstname.length === 0
-    ) {
-      errors.firstname = 'Required'
+    switch (this.state.currentStep) {
+      case 1:
+        if (this.state.values.firstname.length === 0) {
+          errors.firstname = 'Required'
+        }
+        if (
+          this.state.values.firstname.length > 0 &&
+          this.state.values.firstname.length < 5
+        ) {
+          errors.firstname = 'Must be 5 characters or more'
+        }
+        if (this.state.values.lastname.length === 0) {
+          errors.lastname = 'Required'
+        }
+        if (
+          this.state.values.lastname.length > 0 &&
+          this.state.values.lastname.length < 5
+        ) {
+          errors.lastname = 'Must be 5 characters or more'
+        }
+        if (this.state.values.password.length === 0) {
+          errors.password = 'Required'
+        }
+        if (
+          this.state.values.password.length > 0 &&
+          this.state.values.password.length < 6
+        ) {
+          errors.password = 'Must be 6 characters or more'
+        }
+        if (this.state.values.repeatPassword !== this.state.values.password) {
+          errors.repeatPassword = 'Must be equal password'
+        }
+        if (this.state.values.gender.length === 0) {
+          errors.gender = 'Required'
+        }
+        break
+      case 2:
+        if (this.state.values.email.length === 0) {
+          errors.email = 'Required'
+        }
+        if (
+          this.state.values.email.length > 0 &&
+          !mailRe.test(this.state.values.email)
+        ) {
+          errors.email = 'Invalid email'
+        }
+        if (this.state.values.mobile.length === 0) {
+          errors.mobile = 'Required'
+        }
+        if (
+          this.state.values.mobile.length > 0 &&
+          !telRe.test(this.state.values.mobile)
+        ) {
+          errors.mobile = 'Invalid mobile (should be 10 numeric)'
+        }
+        if (this.state.values.country === '0') {
+          errors.country = 'Required'
+        }
+        if (this.state.values.city === '0') {
+          errors.city = 'Required'
+        }
+        break
+      case 3:
+        if (this.state.values.avatar === avatar) {
+          errors.avatar = 'Required'
+        }
+        break
+      default:
+        return null
     }
-    if (
-      this.state.currentStep === 1 &&
-      this.state.values.firstname.length > 0 &&
-      this.state.values.firstname.length < 5
-    ) {
-      errors.firstname = 'Must be 5 characters or more'
-    }
-    if (
-      this.state.currentStep === 1 &&
-      this.state.values.lastname.length === 0
-    ) {
-      errors.lastname = 'Required'
-    }
-    if (
-      this.state.currentStep === 1 &&
-      this.state.values.lastname.length > 0 &&
-      this.state.values.lastname.length < 5
-    ) {
-      errors.lastname = 'Must be 5 characters or more'
-    }
-    if (
-      this.state.currentStep === 1 &&
-      this.state.values.password.length === 0
-    ) {
-      errors.password = 'Required'
-    }
-    if (
-      this.state.currentStep === 1 &&
-      this.state.values.password.length > 0 &&
-      this.state.values.password.length < 6
-    ) {
-      errors.password = 'Must be 6 characters or more'
-    }
-    if (
-      this.state.currentStep === 1 &&
-      this.state.values.repeatPassword !== this.state.values.password
-    ) {
-      errors.repeatPassword = 'Must be equal password'
-    }
-    if (this.state.currentStep === 1 && this.state.values.gender.length === 0) {
-      errors.gender = 'Required'
-    }
-    if (this.state.currentStep === 2 && this.state.values.email.length === 0) {
-      errors.email = 'Required'
-    }
-    if (
-      this.state.currentStep === 2 &&
-      this.state.values.email.length > 0 &&
-      !mailRe.test(this.state.values.email)
-    ) {
-      errors.email = 'Invalid email'
-    }
-    if (this.state.currentStep === 2 && this.state.values.mobile.length === 0) {
-      errors.mobile = 'Required'
-    }
-    if (
-      this.state.currentStep === 2 &&
-      this.state.values.mobile.length > 0 &&
-      !telRe.test(this.state.values.mobile)
-    ) {
-      errors.mobile = 'Invalid mobile (should be 10 numeric)'
-    }
-    if (this.state.currentStep === 2 && this.state.values.country === '0') {
-      errors.country = 'Required'
-    }
-    if (this.state.currentStep === 2 && this.state.values.city === '0') {
-      errors.city = 'Required'
-    }
-    if (this.state.currentStep === 3 && this.state.values.avatar === avatar) {
-      errors.avatar = 'Required'
-    }
+
     return errors
   }
 
